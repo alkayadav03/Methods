c05095ad552b9d414e616810b931fb6b44dfad05
Alka Yadav <yalka834@gmail.com>
2025-08-10 14:54:54 +0000

    Power

---
+++ Power.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Power{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+    //     int a=sc.nextInt();
+    //     int b=sc.nextInt();
+    //     int pow=1;
+    //     for(int i=0;i<=b;i++){
+    //         pow=pow*a;
+    //     }
+    //     System.out.print(pow);
+    // }
+          double a=sc.nextDouble();
+        double b=sc.nextDouble();
+            System.out.print(Math.pow(a,b));
+        }
+}
